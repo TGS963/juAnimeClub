d0b8f995e15750db184c2fb77fd6d101bb07f9b9
TGS963 <ghoshsuvojit2012@gmail.com>
2023-01-20 23:01:57 +0530

    QoL improvements

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -34,7 +34,32 @@ const Gallery = () => {
           );
         })}
       </div>
+      <DisclamerComponent />
     </>
+  );
+};
+
+const DisclamerComponent = () => {
+  return (
+    <div className="mt-16 flex w-full items-center justify-center border-t-2 border-t-yellow-400 bg-gray-900 bg-opacity-30">
+      <p className="flex w-fit flex-col gap-4  p-6 text-center text-yellow-400">
+        Disclaimer: All the posters that are available here or sold by us are-
+        <ol
+          typeof="1"
+          className="flex flex-col gap-2 text-start text-base text-white"
+        >
+          <li>1. Either totally made made by us</li>
+          <li>
+            2. Or made from the images/posters/wallpapers that are free to
+            download and/or use
+          </li>
+          <li>
+            3. Or made by any artist(s) whose permission is taken and credit
+            is given properly
+          </li>
+        </ol>
+      </p>
+    </div>
   );
 };
 
